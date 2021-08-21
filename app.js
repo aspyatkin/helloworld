d911915d6ed05e937d5f422451899b4c8937bce7
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2021-08-21 16:14:51 +0400

    Add timeout

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(3000, () => console.log('Server ready'))
+app.get('/', function (req, res) {
+  let timeout = 0
+  if (Object.prototype.hasOwnProperty.call(req.query, 'timeout')) {
+    timeout = parseInt(req.query.timeout, 10)
+  }
+  setTimeout(function () {
+    res.send('Hello World!')
+  }, timeout)
+})
+
+app.listen(3000, function () {
+  console.log('Server ready')
+})
